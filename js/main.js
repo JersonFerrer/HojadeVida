62462fbf80c0d41c148358eb2bd5cc48d3c9df33
Jerson_Ferrer <jferrer00747@gmail.com>
2020-09-27 17:20:01 -0500

    added hour and tables improved

--- js/main.js
+++ js/main.js
@@ -9,9 +9,6 @@ $(document).ready(function(){
     
     setInterval(() => {
         var date = new Date()
-        //$('#date').html(date);
-        //$('#date').html(date.getDate()+'/'+ (date.getMonth()+1)+'/'+ date.getFullYear()+' '+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds());
         $('#date').html($.datepicker.formatDate('dd/mm/yy', date)+' '+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds());
-
     }, 1000);
 });
