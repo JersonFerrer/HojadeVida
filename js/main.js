44c248961f347dd984fdaaa9b957f30d67f3f62e
Jerson_Ferrer <jferrer00747@gmail.com>
2020-09-27 14:41:06 -0500

    added clock

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-$('#accordion').accordion({
-    collapsible: true,
-    icons: false,
-    heightStyle: "content"
+$(document).ready(function(){
+
+    $('#accordion').accordion({
+        collapsible: true,
+        icons: false,
+        heightStyle: "content"
+    });
+
+    
+    setInterval(() => {
+        var date = new Date()
+        //$('#date').html(date);
+        //$('#date').html(date.getDate()+'/'+ (date.getMonth()+1)+'/'+ date.getFullYear()+' '+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds());
+        $('#date').html($.datepicker.formatDate('dd/mm/yy', date)+' '+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds());
+
+    }, 1000);
 });
