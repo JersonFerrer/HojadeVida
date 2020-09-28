22d7ef5d9e25213a45c4b6c78159c9370274cb45
Jerson_Ferrer <jferrer00747@gmail.com>
2020-09-27 19:18:54 -0500

    added firm

--- js/main.js
+++ js/main.js
@@ -11,4 +11,17 @@ $(document).ready(function(){
         var date = new Date()
         $('#date').html($.datepicker.formatDate('dd/mm/yy', date)+' '+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds());
     }, 1000);
+
+
+    $('#last-name1').keyup(function () {
+        $('#firm').val($('#names').val().toUpperCase()+' '+$(this).val().toUpperCase()+' '+$('#last-name2').val().toUpperCase());
+    });
+
+    $('#last-name2').keyup(function () {
+        $('#firm').val($('#names').val().toUpperCase()+' '+$('#last-name1').val().toUpperCase()+' '+$(this).val().toUpperCase());
+    });
+
+    $('#names').keyup(function () {
+        $('#firm').val($(this).val().toUpperCase()+' '+$('#last-name1').val().toUpperCase()+' '+$('#last-name2').val().toUpperCase());
+    });
 });
